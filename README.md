87e1a4181b75eeb881eb2df6bc55669fa043cb67
unknown <KIIT@CASSINI.CASSINI>
2023-04-07 15:46:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# colorciel
+
+ColorCiel
+ColorCiel is an Android application that helps people with color blindness and those who have difficulty recognizing colors. The app uses the camera on the phone to capture colors, and provides the user with the corresponding RGB value.
+
+Installation
+To install ColorCiel, follow these steps:
+
+Download the APK file from the release section of the GitHub repository.
+Install the APK on your Android device.
+Open the app and allow it to access your camera and storage.
+Usage
+To use ColorCiel, follow these steps:
+
+Open the app and click the "Pick a color" button.
+Point the camera at the object whose color you want to capture.
+The app will show the corresponding RGB value.
+The background color of the app will also change to match the captured color.
+Dependencies
+ColorCiel uses the color_picker_camera package for capturing colors from the camera.
+
+License
+ColorCiel is released under the MIT license. See LICENSE for more details.
+
+Contributing
+If you'd like to contribute to ColorCiel, please open a pull request. All contributions are welcome!
+
+Contact
+If you have any questions or comments, please email me at parag263@gmail.com.
+
+Acknowledgments
+We would like to thank the developers of the color_picker_camera package for providing an easy-to-use color picker.
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:color_picker_camera/color_picker_camera.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false, // Disables the debug banner
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color bgColor = Colors.black;
+  String selectedColor;
+
+  // String colorName = "";//
+
+  @override
+  void initState() {
+    super.initState();
+    selectedColor = ""; // Initializing the selectedColor variable
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text(
+          "ColorCiel",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.w300,
+            fontStyle: FontStyle.italic,
+            letterSpacing: 0.5,
+          ),
+        ),
+        backgroundColor: Colors.grey[800],
+        elevation: 30,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            // colorName ?? "",
+            ":)",
+            // "\"Color is the language of the soul, painting our world with endless possibilities.\"",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          const SizedBox(
+            height: 19,
+          ),
+          Text(
+            selectedColor ?? "",
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              String colorCode = await ColorPickerCamera.captureColorFromCamera;
+              setState(() {
+                selectedColor = colorCode;
+                bgColor = Color(int.parse(colorCode));
+                // colorName = bgColor.toString();
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.grey[800], // Change the button color
+              elevation: 3, // Remove the shadow
+              textStyle: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30,
+                vertical: 15,
+              ),
+              // shape: RoundedRectangleBorder(
+              //   borderRadius: BorderRadius.circular(20),
+              // ),
+            ),
+            child: const Text("Pick a color"),
+          )
+        ],
+      ),
+    );
+  }
+}
